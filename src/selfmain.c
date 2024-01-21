81b5a11ec94d3182894a771bce40f91574fda6c2
H Yi <hlyi@yahoo.com>
2024-01-21 13:57:00 -0600

    fixed self update and makefile

--- src/selfmain.c
+++ src/selfmain.c
@@ -195,7 +195,7 @@ int main(void) {
     // bootloader doesn't run us a second time. We don't need to erase to write
     // zeros. The remainder of the write unit will be set to 1s which should
     // preserve the existing values but its not critical.
-#if ( (! defined(SRAM_BL_SIZE)) || defined(USE_STD_FLASH_BL) )
+#if (! defined(SRAM_BL_SIZE) )
     uint32_t zeros[2] = {0, 0};
     flash_write_words((void *)(BOOTLOADER_K * 1024), zeros, 2);
 #endif
